4cc1d4f8271726272909ed73c789e08cced53ffb
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2019-02-28 13:43:36 +0100

    Improved logging in init command
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- cmd/srcd/cmd/init.go
+++ cmd/srcd/cmd/init.go
@@ -63,6 +63,8 @@ var initCmd = &cobra.Command{
 		if err := daemon.Start(workdir); err != nil {
 			logrus.Errorf("could not start daemon: %s", err)
 		}
+
+		logrus.Info("daemon started")
 	},
 }
 
