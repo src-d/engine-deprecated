5e33a47408ea7d425e5f63fd202a82ae865de1cf
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2019-02-28 12:50:12 +0100

    Added IntegrationSuite struct in newly created cmdtest package
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

---
+++ cmd/test-utils/common.go
@@ -0,0 +1,67 @@
+package cmdtest
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"regexp"
+	"runtime"
+	"strings"
+
+	"github.com/stretchr/testify/suite"
+)
+
+var srcdBin = fmt.Sprintf("../../../build-integration/engine_%s_%s/srcd", runtime.GOOS, runtime.GOARCH)
+
+type IntegrationSuite struct {
+	suite.Suite
+}
+
+func init() {
+	if os.Getenv("SRCD_BIN") != "" {
+		srcdBin = os.Getenv("SRCD_BIN")
+	}
+}
+
+func (s *IntegrationSuite) runCommand(ctx context.Context, cmd string, args ...string) (*bytes.Buffer, error) {
+	args = append([]string{cmd}, args...)
+
+	var out bytes.Buffer
+
+	command := exec.CommandContext(ctx, srcdBin, args...)
+	command.Stdout = &out
+	command.Stderr = &out
+
+	return &out, command.Run()
+}
+
+var logMsgRegex = regexp.MustCompile(`.*msg="(.+?[^\\])"`)
+
+func (s *IntegrationSuite) ParseLogMessages(memLog *bytes.Buffer) []string {
+	var logMessages []string
+	for _, line := range strings.Split(memLog.String(), "\n") {
+		line = strings.TrimSpace(line)
+		if len(line) == 0 {
+			continue
+		}
+
+		match := logMsgRegex.FindStringSubmatch(line)
+		logMessages = append(logMessages, match[1])
+	}
+
+	return logMessages
+}
+
+func (s *IntegrationSuite) RunInit(ctx context.Context, workdir string) (*bytes.Buffer, error) {
+	return s.runCommand(ctx, "init", workdir)
+}
+
+func (s *IntegrationSuite) RunSQL(ctx context.Context, query string) (*bytes.Buffer, error) {
+	return s.runCommand(ctx, "sql", query)
+}
+
+func (s *IntegrationSuite) RunStop(ctx context.Context) (*bytes.Buffer, error) {
+	return s.runCommand(ctx, "stop")
+}
