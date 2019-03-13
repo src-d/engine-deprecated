cca55ecb971ed7328094f1f0d74bc778cb4389ad
Carlos Martín <carlos.martin.sanchez@gmail.com>
2019-03-13 16:51:48 +0100

    Add integration tests for parse cmd
    Signed-off-by: Carlos Martín <carlos.martin.sanchez@gmail.com>

---
+++ cmd/srcd/cmd/testdata/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // include API
+
+using namespace std;
+
+int main() // the main code portion of a C++ program
+{
+   cout << "Hello World" << endl;  //print Hello World on the screen 
+   return 0; // conventional
+}
