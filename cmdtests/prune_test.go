4775b0083a033d39177244969b25ea32f91a41cc
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2019-04-04 13:12:46 +0200

    Added test TestRunningContainersWithImages for testing prune --with-commands command
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- cmdtests/prune_test.go
+++ cmdtests/prune_test.go
@@ -4,34 +4,35 @@ package cmdtests_test
 
 import (
 	"context"
+	"os"
 	"sort"
 	"testing"
 
 	"github.com/src-d/engine/cmdtests"
+	"github.com/src-d/engine/components"
 	"github.com/src-d/engine/docker"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 	"github.com/pkg/errors"
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 )
 
-// This test suite does not test the `--with-images` flag.
+// This test suite by default does not test the `--with-images` flag.
 // If the tests are run with `make test-integration` the daemon image will be
 // the one build locally (e.g. srcd/cli-daemon:dev-b72f1fe), and deleting
 // this image would make all the other tests fail.
+// Running the tests with `TEST_PRUNE_WITH_IMAGE=true make test-integration`
+// will run the test with `--with-images` flag as last in order to avoid the
+// aforementioned problem.
 
 type PruneTestSuite struct {
 	cmdtests.IntegrationTmpDirSuite
 }
 
-func TestPruneTestSuite(t *testing.T) {
-	s := PruneTestSuite{}
-	suite.Run(t, &s)
-}
-
-func (s *PruneTestSuite) TestRunningContainers() {
-	require := s.Require()
+func (s *PruneTestSuite) testRunningContainers(require *require.Assertions, withImages bool) {
+	s.T().Helper()
 
 	// Get the list of volumes and networks before calling init
 	prevVols, err := docker.ListVolumes(context.Background())
@@ -46,7 +47,12 @@ func (s *PruneTestSuite) TestRunningContainers() {
 	r = s.RunCommand("sql", "SELECT 1")
 	require.NoError(r.Error, r.Combined())
 
-	r = s.RunCommand("prune")
+	if withImages {
+		r = s.RunCommand("prune", "--with-images")
+	} else {
+		r = s.RunCommand("prune")
+	}
+
 	require.NoError(r.Error, r.Combined())
 
 	// Test containers were deleted
@@ -77,6 +83,56 @@ func (s *PruneTestSuite) TestRunningContainers() {
 	require.Equal(netNames(prevNets), netNames(nets))
 }
 
+func (s *PruneTestSuite) TestRunningContainers() {
+	s.testRunningContainers(s.Require(), false)
+}
+
+func (s *PruneTestSuite) TestStoppedContainers() {
+	require := s.Require()
+
+	r := s.RunCommand("prune")
+	require.NoError(r.Error, r.Combined())
+}
+
+func (s *PruneTestSuite) TestRunningContainersWithImages() {
+	if os.Getenv("TEST_PRUNE_WITH_IMAGE") != "true" {
+		s.T().Skip("Use env var TEST_PRUNE_WITH_IMAGE=true to test srcd prune with --with-images flag")
+	}
+
+	s.testRunningContainers(s.Require(), true)
+	s.requireNoImages()
+}
+
+func (s *PruneTestSuite) requireNoImages() {
+	s.T().Helper()
+	require := s.Require()
+
+	images, _ := docker.ListImages(context.Background())
+	imagesNames := make(map[string]components.Component)
+	for _, c := range []components.Component{
+		components.Daemon,
+		components.Gitbase,
+		components.GitbaseWeb,
+		components.MysqlCli,
+		components.Bblfshd,
+		components.BblfshWeb,
+	} {
+		imagesNames[c.Image] = c
+	}
+
+	for _, i := range images {
+		for _, rt := range i.RepoTags {
+			img, _ := docker.SplitImageID(rt)
+			comp, ok := imagesNames[img]
+			require.Falsef(ok, "Image with repo tag '%s' shouldn't be present for component '%s'", rt, comp.Name)
+		}
+	}
+}
+
+func TestPruneTestSuite(t *testing.T) {
+	suite.Run(t, &PruneTestSuite{})
+}
+
 func volNames(volumes []*docker.Volume) []string {
 	var names []string
 	for _, vol := range volumes {
@@ -104,11 +160,4 @@ func listNetworks() ([]types.NetworkResource, error) {
 	}
 
 	return c.NetworkList(context.Background(), types.NetworkListOptions{})
-}
-
-func (s *PruneTestSuite) TestStoppedContainers() {
-	require := s.Require()
-
-	r := s.RunCommand("prune")
-	require.NoError(r.Error, r.Combined())
 }
