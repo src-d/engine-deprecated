a208682784efb4a4cfa15d4b0bb57341cede7f19
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2019-04-25 13:08:05 +0200

    Align test timeout between Makefile and bat file
    Tests ran the bat file for Windows uses the default timeout of
    10m. This has to be aligned with the Makefile that uses 20m.
    
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- run-integration-tests.bat
+++ run-integration-tests.bat
@@ -12,4 +12,4 @@ go build -tags=forceposix -o build/engine_windows_amd64/srcd.exe ./cmd/srcd
 
 :: run tests
 set SRCD_BIN=../build/engine_windows_amd64/srcd.exe
-go test -parallel 1 -count 1 -tags="forceposix integration" github.com/src-d/engine/cmdtests/ -v
+go test -timeout 20m -parallel 1 -count 1 -tags="forceposix integration" github.com/src-d/engine/cmdtests/ -v
